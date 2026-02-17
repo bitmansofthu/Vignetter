a5a4e7f7fa1912f6418637e7166e0ace22356451
bitmansofthu <aquajava@gmail.com>
2026-02-17 21:47:02 +0100

    Initial Commit

---
+++ Vignetter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vignetter
+//
+//  Created by Ferenc Knebl on 2026. 02. 17..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vignetter/VignetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VignetterApp.swift
+//  Vignetter
+//
+//  Created by Ferenc Knebl on 2026. 02. 17..
+//
+
+import SwiftUI
+
+@main
+struct VignetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
